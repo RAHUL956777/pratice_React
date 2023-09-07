8007b6127fe9836c7cc9ad5a5607aa799e006ec8
RAHUL <dasr16583@gmail.com>
2023-09-07 20:54:47 +0530

    eorking done on currency converter

--- 3_customHooks-currencyconverter/src/App.jsx
+++ 3_customHooks-currencyconverter/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { InputBox } from "./components";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
-import "./App.css"
+
 
 function App() {
   const [amount, setAmount] = useState(0);
@@ -26,13 +26,13 @@ function App() {
 
   return (
     <div
-      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      className="w-screen h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
         backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
       }}
     >
       <div className="w-full">
-        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+        <div className="w-full h-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <form
             onSubmit={(e) => {
               e.preventDefault();
