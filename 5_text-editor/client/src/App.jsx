e2d96d051d2c4cd4e6599764478c9f9e11bfce87
RAHUL <dasr16583@gmail.com>
2023-09-10 19:27:34 +0530

    working on text editor project ushing quil.js

---
+++ 5_text-editor/client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TextEditor from './TextEditor'
+import "./styles.css"
+
+const App = () => {
+  return (
+    <div>
+      <TextEditor />
+    </div>
+  )
+}
+
+export default App
+
---
+++ 5_text-editor/client/src/TextEditor.jsx
@@ -0,0 +1,30 @@
+import React, { useCallback, useRef } from "react";
+import "quill/dist/quill.snow.css";
+import Quill from "quill";
+
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "itelic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
+
+const TextEditor = () => {
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS } });
+  }, []);
+
+  return <div className="container" ref={wrapperRef}></div>;
+};
+
+export default TextEditor;
