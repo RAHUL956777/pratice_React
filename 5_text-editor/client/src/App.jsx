de1f1f90a4ebeaa5af415b232e65930ea09e8a4a
RAHUL <dasr16583@gmail.com>
2023-09-11 10:52:34 +0530

    working on routung v6.15

--- 5_text-editor/client/src/App.jsx
+++ 5_text-editor/client/src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import TextEditor from './TextEditor'
-import "./styles.css"
+import React from "react";
+import TextEditor from "./TextEditor";
+import "./styles.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidv4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
-
+export default App;
