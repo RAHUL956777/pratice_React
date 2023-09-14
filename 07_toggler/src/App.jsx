d769c2f66ad43bb20d348fd1766d310cf0a72876
RAHUL <dasr16583@gmail.com>
2023-09-14 18:45:27 +0530

    context api

--- 07_toggler/src/App.jsx
+++ 07_toggler/src/App.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./context/Theme";
+import ThemeButton from "./components/ThemeButton";
+import Card from "./components/Card";
 
 function App() {
-  return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+  const [themeMode, setThemeMode] = useState("light");
 
-        <div className="w-full max-w-sm mx-auto"></div>
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // changing background ushing useEffect
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeButton />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
