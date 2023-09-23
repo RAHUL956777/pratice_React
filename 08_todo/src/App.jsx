1403acd7bce86bc09e771c0ada79c82bf1b8b452
RAHUL <dasr16583@gmail.com>
2023-09-23 10:04:26 +0530

    working done on cntext api

--- 08_todo/src/App.jsx
+++ 08_todo/src/App.jsx
@@ -5,9 +5,38 @@ import { TodoProvider } from "./contexts/TodoContext";
 function App() {
   const [todos, setTodos] = useState([]);
 
-  const addTodo=(todo)=>{
-    
-  }
+  const addTodo = (todo) => {
+    setTodos((prev) => [
+      {
+        id: Date.now(),
+        ...todo,
+      },
+      ...prev,
+    ]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? {
+              ...prevTodo,
+              completed: !prevTodo.completed,
+            }
+          : prevTodo
+      )
+    );
+  };
 
   return (
     <TodoProvider
