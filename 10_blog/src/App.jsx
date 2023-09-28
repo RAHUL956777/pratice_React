5e9b9579e20addf3a17caee191299073980706d4
RAHUL <dasr16583@gmail.com>
2023-09-28 22:12:20 +0530

    react-redux(slices,storage) done

--- 10_blog/src/App.jsx
+++ 10_blog/src/App.jsx
@@ -1,14 +1,39 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import {Header,Footer} from "./components";
+// import { Outlet } from "react-router-dom";
 
 function App() {
-  
-console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
+  const [loading, setLoading] = useState(true);
+  const dispacth = useDispatch();
 
-    <>
-    <h1>hello</h1>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispacth(login({ userData }));
+        } else {
+          dispacth(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  });
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
-export default App
+export default App;
