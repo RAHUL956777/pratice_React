92dadaaa88c822138d3fc7bc77ebf8a39d089acd
RAHUL <dasr16583@gmail.com>
2023-09-29 23:35:26 +0530

    login component done

---
+++ 10_blog/src/components/Signup.jsx
@@ -0,0 +1,15 @@
+import React,{useState} from "react"
+import authService from "../appwrite/auth"
+import {Link,useNavigate} from "react-router-dom"
+import {Login} from '../store/authSlice'
+
+
+function Signup() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Signup
