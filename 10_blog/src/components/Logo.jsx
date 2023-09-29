53e952ada2c7c2930fc5d1fa067ad017f20fe80d
RAHUL <dasr16583@gmail.com>
2023-09-29 10:33:39 +0530

    working on footer component

---
+++ 10_blog/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+
+
+function Logo({ width = "100px" }) {
+  return <div>Logo</div>;
+}
+
+export default Logo;
---
+++ 10_blog/src/components/container/Container.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Container = () => {
+  return <div className="w-full max-w-7 mx-auto px-4 ">{children}</div>;
+};
+
+export default Container;
