19e68dd8d41f89dd148ebff9c14b407e7d5f2e4f
RAHUL <dasr16583@gmail.com>
2023-10-01 10:07:07 +0530

    working on postForm components

--- 10_blog/src/components/container/Container.jsx
+++ 10_blog/src/components/container/Container.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 
-const Container = () => {
+const Container = ({children}) => {
   return <div className="w-full max-w-7 mx-auto px-4 ">{children}</div>;
 };
 
--- 10_blog/src/components/index.js
+++ 10_blog/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn,RTE };
