cb6bcf940018cc9457e1af6352020451f4a3f3a8
RAHUL <dasr16583@gmail.com>
2023-10-20 18:22:10 +0530

    initial commit

---
+++ proj1/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
