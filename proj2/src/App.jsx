44462a4e39fb0d86d1b881148dde4fdeb59f9060
RAHUL <dasr16583@gmail.com>
2023-10-26 19:30:59 +0530

    working done on header

--- proj2/src/App.jsx
+++ proj2/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route />
       </Routes>
