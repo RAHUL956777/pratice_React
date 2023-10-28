3e72995b9b7e5f86dcc33bdd76d740d527d72b0a
RAHUL <dasr16583@gmail.com>
2023-10-28 18:53:49 +0530

    working on header component

--- proj3/src/components/Header.jsx
+++ proj3/src/components/Header.jsx
@@ -1,11 +1,20 @@
-
+import { Button, HStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <HStack p={"4"} shadow={"base"} backgroundColor={"blackAlpha.900"}>
+      <Button variant={"unstyled"} color={"white"}>
+        <Link to={"/"}>Home</Link>
+      </Button>
+      <Button variant={"unstyled"} color={"white"}>
+        <Link to={"/exchanges"}>Exchanges</Link>
+      </Button>
+      <Button variant={"unstyled"} color={"white"}>
+        <Link to={"/coins"}>Coins</Link>
+      </Button>
+    </HStack>
+  );
+};
 
-export default Header
+export default Header;
