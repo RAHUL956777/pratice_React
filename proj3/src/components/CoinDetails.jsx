b1753e0ba9ef3031c228287e5e2b049b3c6907bf
RAHUL <dasr16583@gmail.com>
2023-10-30 20:25:56 +0530

    working on other deltails of coins components ushing api

--- proj3/src/components/CoinDetails.jsx
+++ proj3/src/components/CoinDetails.jsx
@@ -1,11 +1,15 @@
 import {
+  Badge,
   Box,
   Container,
   HStack,
   Image,
+  Progress,
   Radio,
   RadioGroup,
   Stat,
+  StatArrow,
+  StatHelpText,
   StatLabel,
   StatNumber,
   Text,
@@ -19,16 +23,14 @@ import axios from "axios";
 import { server } from "../main.jsx";
 import Error from "./Error.jsx";
 
-
 const CoinDetails = () => {
   const [coin, setCoin] = useState({});
   const [loading, SetLoading] = useState(true);
   const [error, setError] = useState(false);
   const [currency, setCurrency] = useState("inr");
 
-  
-const currencySymbol =
-currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
+  const currencySymbol =
+    currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
 
   const params = useParams();
 
@@ -84,11 +86,76 @@ currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
                 {currencySymbol}
                 {coin.market_data.current_price[currency]}
               </StatNumber>
+              <StatHelpText>
+                <StatArrow
+                  type={
+                    coin.market_data.price_change_percentage_24h > 0
+                      ? "increase"
+                      : "decrease"
+                  }
+                />
+                {coin.market_data.price_change_percentage_24h}%
+              </StatHelpText>
             </Stat>
+            <Badge
+              fontSize={"2xl"}
+              bgColor={"blackAlpha.900"}
+              color={"white"}
+            >{`#${coin.market_cap_rank}`}</Badge>
+
+            <CustomBar
+              high={`${currencySymbol}${coin.market_data.high_24h[currency]}`}
+              low={`${currencySymbol}${coin.market_data.low_24h[currency]}`}
+            />
+
+            <Box w={"full"} p="4">
+              <Item title={"Max Supply"} value={coin.market_data.max_supply} />
+              <Item
+                title={"Circulateing Supply"}
+                value={coin.market_data.circulating_supply}
+              />
+              <Item
+                title={"Market Cap"}
+                value={`${currencySymbol}${coin.market_data.market_cap[currency]}}`}
+              />
+              <Item
+                title={"All Time Low"}
+                value={`${currencySymbol}${coin.market_data.atl[currency]}}`}
+              />
+              <Item
+                title={"All Time High"}
+                value={`${currencySymbol}${coin.market_data.ath[currency]}}`}
+              />
+            </Box>
           </VStack>
         </>
       )}
     </Container>
+  );
+};
+
+const Item = ({ title, value }) => {
+  return (
+    <HStack justifyContent={"space-between"} w={"full"} my={"4"}>
+      <Text fontFamily={"Bebas Neue"} letterSpacing={"widest"}>
+        {title}
+      </Text>
+      <Text>{value}</Text>
+    </HStack>
+  );
+};
+
+const CustomBar = ({ high, low }) => {
+  return (
+    <VStack w={"full"}>
+      <Progress value={50} colorScheme={"teal"} w={"full"} />
+      <HStack justifyContent={"space-evenly"} w={"full"}>
+        <Badge colorScheme="red">{low}</Badge>
+        <Badge colorScheme="green">{high}</Badge>
+
+        <Text fontSize={"sm"}>24H Range</Text>
+      </HStack>
+    </VStack>
   );
 };
 
