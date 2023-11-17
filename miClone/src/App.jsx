c7195027810bd57ee7c2a19638c96e8a488456b8
RAHUL <dasr16583@gmail.com>
2023-11-17 09:26:55 +0530

    working on others component

--- miClone/src/App.jsx
+++ miClone/src/App.jsx
@@ -3,20 +3,37 @@ import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
 import Offers from "./components/Offers";
-import data from  "./data/data.json";
+import data from "./data/data.json";
 import Heading from "./components/Heading";
-import StarProduct from "./components/StarProduct.jsx"
-import { BrowserRouter as Router } from "react-router-dom";
+import StarProduct from "./components/StarProduct";
+import HotAccessoriesMenu from "./components/HotAccessoriesMenu";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HotAccessories from "./components/HotAccessories";
 
 function App() {
   return (
     <Router>
-      <PreNavbar />
-      <Navbar />
-      <Slider start={data.banner.start}/>
-      <Offers offer={data.offer}/>
-      <Heading text="STAR PRODUCTS"/>
-      <StarProduct starProduct={data.starProduct}/>
+      <Routes>
+        <Route path="/">
+          <PreNavbar />
+          <Navbar />
+          <Slider start={data.banner.start} />
+          <Offers offer={data.offer} />
+          <Heading text="STAR PRODUCTS" />
+          <StarProduct starProduct={data.starProduct} />
+          <Heading text="HOT ACCESSORIES" />
+          <HotAccessoriesMenu />
+        </Route>
+
+        <Route exact path="/music">
+        <HotAccessories
+          music={data.hotAccessories.music}
+          musicCover={data.hotAccessoriesCover.music}
+        />
+      </Route>
+      </Routes>
+
+      
     </Router>
   );
 }
---
+++ miClone/src/components/HotAccessories.jsx
@@ -0,0 +1,26 @@
+import HotItemCard from "./HotItemCard";
+import "../styles/HotAccessories.css";
+
+const HotAccessories = ({ music, musicCover }) => {
+  return (
+    <div className="HotAccessories">
+      <div>
+        <img src={musicCover} alt="cover" />
+      </div>
+
+      <div>
+        {music.map((item, index) => (
+          <HotItemCard
+            key={item.name}
+            name={item.name}
+            price={item.price}
+            image={item.price}
+            index={index}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HotAccessories;
