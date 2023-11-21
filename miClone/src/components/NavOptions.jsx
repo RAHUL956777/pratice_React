3080ffaa69095833556381a7234532b3754d7ab1
RAHUL <dasr16583@gmail.com>
2023-11-21 10:23:25 +0530

    working on navOptions component

--- miClone/src/components/NavOptions.jsx
+++ miClone/src/components/NavOptions.jsx
@@ -1,4 +1,7 @@
 import NavCard from "./NavCard";
+import {useState,useEffect} from "react";
+import "../styles/NavOptions.css";
+
 
 const NavOptions = ({
   miphones,
@@ -10,16 +13,55 @@ const NavOptions = ({
   accessories,
   audio,
 }) => {
+
+    const [miPhoneToggle, setMiPhoneToggle] = useState(false);
+    const [redmiPhoneToggle, setRedmiPhoneToggle] = useState(false);
+    const [tvToggle, setTvToggle] = useState(false);
+    const [laptopToggle, setLaptopToggle] = useState(false);
+    const [fitnessToggle, setFitnessToggle] = useState(false);
+    const [homeToggle, setHomeToggle] = useState(false);
+    const [audioToggle, setAudioToggle] = useState(false);
+    const [accessoriesToggle, setAccessoriesToggle] = useState(false);
+
+
+    useEffect(()=>{
+      if(window.location.pathname ==="/miphones"){
+        return setMiPhoneToggle(true);
+      }
+      if(window.location.pathname ==="/redmiphones"){
+        return setRedmiPhoneToggle(true);
+      }
+      if(window.location.pathname ==="/tv"){
+        return setTvToggle(true);
+      }
+      if(window.location.pathname ==="/laptop"){
+        return setLaptopToggle(true);
+      }
+      if(window.location.pathname ==="/fitness"){
+        return setFitnessToggle(true);
+      }
+      if(window.location.pathname ==="/home"){
+        return setHomeToggle(true);
+      }
+      if(window.location.pathname ==="/audio"){
+        return setAudioToggle(true);
+      }
+      if(window.location.pathname ==="/accessories"){
+        return setAccessoriesToggle(true);
+      }
+
+    },[])
+
     return (
         <div className="navOptions"> 
 
-            {miPhoneToggle?   miPhones.map((item)=>(
+            {miPhoneToggle?   miphones.map((item)=>(
 
                  < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
               )) : null }
             
       
-            {redmiPhoneToggle?   redmiPhones.map((item)=>(
+            {redmiPhoneToggle?   redmiphones.map((item)=>(
 
                < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
            )) : null }
--- miClone/src/components/Navbar.jsx
+++ miClone/src/components/Navbar.jsx
@@ -1,43 +1,42 @@
 import "../styles/nav.css";
 import { logo } from "../data/data.json";
-import { Link } from "react-router-dom";
 import { searchIcon } from "../assets/index";
 
 const Navbar = () => {
   return (
     <div className="nav">
       <div className="logo">
-        <Link to="/">
+        <a href="/">
           <img src={logo} alt="logo" id="logoImage"/>
-        </Link>
+        </a>
         </div>
-        <Link className="navlinks" to="/miphones">
+        <a className="navlinks" href="/miphones">
           Mi Phones
-        </Link>
-        <Link className="navlinks" to="/redmiphones">
+        </a>
+        <a className="navlinks" href="/redmiphones">
           Redmi Phones
-        </Link>
-        <Link className="navlinks" to="/tv">
+        </a>
+        <a className="navlinks" href="/tv">
           TV
-        </Link>
-        <Link className="navlinks" to="/laptops">
+        </a>
+        <a className="navlinks" href="/laptops">
           Laptops
-        </Link>
-        <Link className="navlinks" to="/lifestyle">
+        </a>
+        <a className="navlinks" href="/lifestyle">
           Fitness & Lifestyle
-        </Link>
-        <Link className="navlinks" to="/home">
+        </a>
+        <a className="navlinks" href="/home">
           {" "}
           Home
-        </Link>
-        <Link className="navlinks" to="/audio">
+        </a>
+        <a className="navlinks" href="/audio">
           {" "}
           Radio
-        </Link>
-        <Link className="navlinks" to="/accessories">
+        </a>
+        <a className="navlinks" href="/accessories">
           {" "}
           Accessories
-        </Link>
+        </a>
 
         <div className="searchbox">
           <input type="text" placeholder="Search products" />
