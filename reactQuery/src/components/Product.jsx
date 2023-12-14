2aa76fa0865e4d8f488dc3112283f76290d1cb43
RAHUL <dasr16583@gmail.com>
2023-12-14 19:35:29 +0530

    learning mutation in react query

--- reactQuery/src/components/Product.jsx
+++ reactQuery/src/components/Product.jsx
@@ -1,15 +1,57 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
 const Product = () => {
   const params = useParams();
 
-  const fecthProcuct = async()=>{
-    const response = await fecthProcuct(`https://dummyjson.com/products/${productId}`)
-    const data = await response.json();
-    return data;
+  // mutation
+  const mutation = useMutation({
+    mutationFn: (newProduct) => {
+      const res = axios.put(`https://dummyjson.com/products/1`, newProduct);
+      return res;
+    },
+  });
+
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(
+        `https://dummyjson.com/products/${params.productId}`
+      );
+
+      return response;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  const {
+    isLoading,
+    error,
+    data: product,
+  } = useQuery({
+    queryKey: ["product", params.productId],
+    queryFn: fetchProduct,
+  });
+
+  if (error) {
+    return <h3>Error: {error.message}</h3>;
   }
 
-  return <div></div>;
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  return (
+    <>
+      <div>{product.title}</div>;
+      <button
+        onClick={() => {
+          mutation.mutate({ title: "Update Product" });
+        }}
+      ></button>
+    </>
+  );
 };
 
 export default Product;
